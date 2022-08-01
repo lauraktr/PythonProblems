34f2479228a3ba15e993a7131de210ae4901932a
lauraktr <tranlaurakim@gmail.com>
2022-08-01 20:35:54 +0000

    finished blanks problem

--- Blanks.py
+++ Blanks.py
@@ -1,4 +1,44 @@
+def blanks(array, index):
+    indexes = []
+
+    for i in range(len(array)):
+        #print(i)
+        for j in range(len(array[i])):
+            #print("\t", j)
+
+            if array[i][j] == " ":
+                indexes.append([i,j])
+
+    print(indexes)
+
+    print(index) 
+
+    if index in indexes:
+        blank = True
+
+    else:
+        blank = False
+
+    print(blank)
 
 
 if __name__ == '__main__':
     print("Blanks")
+
+
+
+
+    array_1 = [["s", "m" ," ", "h"],
+              ["h", " ", " ", "s"],         
+              ["m", " ", "m", "m"]]
+
+    index_1 = [0, 0]
+
+    array_2 = [[" ", " ", "r"],
+               [" ", " ", "p"]]
+
+    index_2 = [0, 0]
+
+    blanks(array_1, index_1)
+    blanks(array_2, index_2)
+
--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,40 @@
+import numpy as np
 
+def fizzbuzz(x):
+
+   
+    if type(x) is int:
+        if x%5 == 0 and x%3 == 0:
+            return "FizzBuzz"
+        elif x%3 == 0:
+            return "Fizz"
+        elif x%5 == 0:
+            return "Buzz"
+        else:
+            return x
+
+    else:
+        new_x = np.zeros(x.shape)
+        
+        print(new_x)
+
+        for i in range(0, x.shape[0]):
+            if i%5 == 0 and x%3 == 0:
+                new_x[i] = "FizzBuzz"
+            elif i%3 == 0:
+                new_x[i] = "Fizz"
+            elif i%5 == 0:
+                new_x[i] = "Buzz"
+            else:
+                new_x[i] = x
+
+    return new_x
 
 if __name__ == '__main__':
-    print("FizzBuzz")
+    #print("FizzBuzz")
+    print(fizzbuzz(5))
+    print(fizzbuzz(np.array([1,15,66,27,50])))
+
+
+
+
